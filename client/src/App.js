9adbd3e3e7e2f0a0a3212a181664401630d95c82
Astone Tuhame <taentastone@gmail.com>
2022-07-29 14:25:12 +0300

    Antd and bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+import 'antd/dist/antd.css';
+import {Button} from 'antd'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App p-5">
       <h1>Doctor Appointment</h1>
+      <Button type="primary">Primary Button</Button>
     </div>
   );
 }
