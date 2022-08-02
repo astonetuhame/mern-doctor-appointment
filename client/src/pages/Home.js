7c04c222d868e310e77efd7800f6b0ccb0d19057
astonetuhame <astonetuhame@gmail.com>
2022-08-02 15:37:55 +0300

    Authorization with jwt

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import axios from "axios"
+import { useEffect } from "react"
+
 
 function Home() {
+
+    const getData = async() => {
+        try {
+            const response = await axios.post('/api/user/get-user-info-by-id', {}, {
+                headers: {
+                    Authorization: 'Bearer ' + localStorage.getItem('token')
+                }
+            })
+        } catch (error) {
+            
+        }
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
   return (
     <div>Home</div>
   )
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const authMiddleware = require("../middlewares/authMiddleware")
 
 router.post("/register", async (req, res) => {
   const userExists = await User.findOne({ email: req.body.email });
@@ -56,5 +57,23 @@ router.post("/login", async (req, res) => {
     .send({ message: "Error logging in", success: false, error });
   }
 });
+
+router.post('/get-user-info-by-id', authMiddleware, async (req, res) => { 
+
+    try {
+        const user = await User.findOne({ _id: req.body.userId})
+        if(!user){
+            return res.status(200).send({message: "User does not exist", success: false})
+        } else {
+            res.status(200).send({ success: true, data: {
+                name: user.name,
+                email: user.email
+            }})
+        }
+    } catch (error) {
+        res.status(500).send({message: "Error getting user info", success: false, error})
+        
+    }
+ })
 
 module.exports = router;
